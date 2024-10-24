a50b98ead25704ebe7d8b7d87b60b26e1c594862
b22039 <e1b22039@oit.ac.jp>
2024-10-24 17:40:36 +0900

    なにこれわからん

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 5;
+    public UDPReceive udpReceive;
+    public GameObject Player;
     private Rigidbody rb;
     private Vector3 right = new Vector3(3.3f,0,0);
-    private Vector3 left = new Vector3(-3.3f,0,0);  
+    private Vector3 left = new Vector3(-3.3f,0,0);
+    private Vector3 mid = new Vector3(0, 0, 0);
     void FixedUpdate(){
         rb =this.GetComponent<Rigidbody>();
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
@@ -17,16 +21,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+        string data = udpReceive.data;
+
+        if (data ==  "left"){
             if(this.transform.position.x < 0 ) {
                 return;
             }
-            rb.MovePosition(rb.position + left);
-        }else if(Input.GetKeyDown(KeyCode.RightArrow)){
+            transform.position = new Vector3(-3.3f, transform.position.y, transform.position.z);
+        }else if(data == "right"){
             if(this.transform.position.x > 0 ) {
                 return;
             }
-            rb.MovePosition(rb.position + right);
+            transform.position = new Vector3(3.3f, transform.position.y, transform.position.z);
+        }
+        else if(data == "middle"){
+            if (this.transform.position.x == 0)
+            {
+                return;
+            }
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
         }
 
     }
