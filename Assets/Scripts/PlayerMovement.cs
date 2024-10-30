be3e03f3e84875e01988dcca47defa7245f484a2
ok2580 <gomo20031119@gmail.com>
2024-10-30 10:09:00 +0900

    been able to DebugPlay

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float speed = 5;
+    public int DebugPlay =1;
     public UDPReceive udpReceive;
     public GameObject Player;
     private Rigidbody rb;
@@ -18,29 +18,43 @@ public class PlayerMovement : MonoBehaviour
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + forwardMove);
     }
-    // Update is called once per frame
     void Update()
     {
         string data = udpReceive.data;
+        if(DebugPlay == 1){
+            Debug.Log(data);
+            if(Input.GetKeyDown(KeyCode.LeftArrow)){
+                if(this.transform.position.x < 0 ) {
+                    return;
+                }
+                rb.MovePosition(rb.position + left);
+            }else if(Input.GetKeyDown(KeyCode.RightArrow)){
+                if(this.transform.position.x > 0 ) {
+                    return;
+                }
+                rb.MovePosition(rb.position + right);
+            }
+        }else{
+            if (data ==  "left"){
+                if(this.transform.position.x < 0 ) {
+                    return;
+                }
+                transform.position = new Vector3(-3.3f, transform.position.y, transform.position.z);
+            }else if(data == "right"){
+                if(this.transform.position.x > 0 ) {
+                    return;
+                }
+                transform.position = new Vector3(3.3f, transform.position.y, transform.position.z);
+            }
+            else if(data == "middle"){
+                if (this.transform.position.x == 0)
+                {
+                    return;
+                }
+                transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            }
+        }
 
-        if (data ==  "left"){
-            if(this.transform.position.x < 0 ) {
-                return;
-            }
-            transform.position = new Vector3(-3.3f, transform.position.y, transform.position.z);
-        }else if(data == "right"){
-            if(this.transform.position.x > 0 ) {
-                return;
-            }
-            transform.position = new Vector3(3.3f, transform.position.y, transform.position.z);
-        }
-        else if(data == "middle"){
-            if (this.transform.position.x == 0)
-            {
-                return;
-            }
-            transform.position = new Vector3(0, transform.position.y, transform.position.z);
-        }
 
     }
 }
